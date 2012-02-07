0aa80c1cf0f25f5fe82b42fb8bf3fbff9a9c2ed8
Martin K. Scherer <martin.scherer@gcsc.uni-frankfurt.de>
2012-02-07 15:27:22 +0000

    added plane plane intersection
    git-svn-id: svn://fuji/ug4/trunk@4479 babdd8d3-5941-4521-913b-54747f08a64b

---
+++ util/plane_plane_intersection.h
@@ -0,0 +1,38 @@
+/*
+ * plane_plane_intersection.h
+ *
+ *  Created on: 07.02.2012
+ *      Author: marscher
+ */
+
+#ifndef PLANE_PLANE_INTERSECTION_H_
+#define PLANE_PLANE_INTERSECTION_H_
+#include "common/math/math_vector_matrix/math_vector_functions.h"
+#include "common/math/misc/math_util.h"
+
+/**
+ * @param pOut stores support vector of intersection line
+ * @param nOut stores normal vector of intersection line
+ * @param p1 support vector of plane 1
+ * @param n1 normal vector of plane 1
+ * @param p2 support vector of plane 2
+ * @param n2 normal vector of plane 1
+ * @return true, if planes intersect; false if planes are parallel
+ */
+bool PlanePlaneIntersection3D(vector3& pOut, vector3& nOut, const vector3& p1,
+		const vector3& n1, const vector3& p2, const vector3& n2) {
+
+	// check planes are not parallel
+	// which is the case iff | n1 * n2 | = 1
+	if (fabs(VecDot(n1, n2) - 1) < SMALL)
+		return false;
+
+	// intersection normal perpendicular to n1 and n2
+	VecCross(nOut, n1, n2);
+
+	// dertermine point on intersection line
+	RayPlaneIntersection(pOut, p1, nOut, p2, n2);
+	return true;
+}
+
+#endif /* PLANE_PLANE_INTERSECTION_H_ */
