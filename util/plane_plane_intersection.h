f630d25952114c01ec9f8a4c19382be5d6cb16f6
Martin K. Scherer <martin.scherer@gcsc.uni-frankfurt.de>
2012-02-07 15:43:19 +0000

    normalized input "normals" prior checking if they are orthogonal return status of RayPlaneIntersection
    git-svn-id: svn://fuji/ug4/trunk@4483 babdd8d3-5941-4521-913b-54747f08a64b

--- util/plane_plane_intersection.h
+++ util/plane_plane_intersection.h
@@ -12,27 +12,32 @@
 
 /**
  * @param pOut stores support vector of intersection line
- * @param nOut stores normal vector of intersection line
+ * @param dOut stores the direction vector of the intersection line
  * @param p1 support vector of plane 1
  * @param n1 normal vector of plane 1
  * @param p2 support vector of plane 2
  * @param n2 normal vector of plane 1
  * @return true, if planes intersect; false if planes are parallel
  */
-bool PlanePlaneIntersection3D(vector3& pOut, vector3& nOut, const vector3& p1,
+bool PlanePlaneIntersection3D(vector3& pOut, vector3& dOut, const vector3& p1,
 		const vector3& n1, const vector3& p2, const vector3& n2) {
 
 	// check planes are not parallel
 	// which is the case iff | n1 * n2 | = 1
-	if (fabs(VecDot(n1, n2)) - 1 < SMALL)
+	vector3 tn1, tn2;
+
+	VecNormalize(tn1, n1);
+	VecNormalize(tn2, n2);
+
+	if (fabs(VecDot(tn1, tn2)) - 1 < SMALL)
 		return false;
 
 	// intersection normal perpendicular to n1 and n2
-	VecCross(nOut, n1, n2);
+	VecCross(dOut, n1, n2);
 
 	// dertermine point on intersection line
-	RayPlaneIntersection(pOut, p1, nOut, p2, n2);
-	return true;
+	number tmp;
+	return RayPlaneIntersection(pOut, tmp, p1, dOut, p2, n2);
 }
 
 #endif /* PLANE_PLANE_INTERSECTION_H_ */
