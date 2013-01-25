fe3cf24cfb427fbe0e1699ecfbb1e806b00419ed
mscherer <martin.scherer@gcsc.uni-frankfurt.de>
2013-01-25 18:39:30 +0100

    adopted plugin main of tkd gen to latest layout. compile it only for 3d

--- plugin_main.cpp
+++ plugin_main.cpp
@@ -5,98 +5,143 @@
  *      Author: marscher
  */
 
-#include "registry/registry.h"
-
+#include "bridge/util.h"
+#include "bridge/util_domain_dependent.h"
 #include "domain_generator.h"
 #include "geometry_generator.h"
 
-using namespace ug;
+#include <boost/mpl/list.hpp>
+#include <string>
 
-// register tkd generator functions for usage in ug_script
-extern "C" void
-InitUGPlugin_TKDGenerator(bridge::Registry* reg, const std::string& parentGroup) {
-	std::string grp(parentGroup);
+using std::string;
+using namespace ug::bridge;
+
+namespace ug {
+namespace tkd {
+
+/**
+ * Class exporting the functionality. All functionality that is to
+ * be used in scripts or visualization must be registered here.
+ */
+struct Functionality
+{
+
+/**
+ * Function called for the registration of Dimension dependent parts.
+ * All Functions and Classes depending on the Dimension
+ * are to be placed here when registering. The method is called for all
+ * available Dimension types, based on the current build options.
+ *
+ * @param reg				registry
+ * @param parentGroup		group for sorting of functionality
+ */
+template <typename TDom>
+static void Domain(Registry& reg, string grp)
+{
 	grp.append("tkd_generator/");
 
 	typedef ug::tkd::TKDGeometryGenerator geomGen;
 	typedef ug::tkd::TKDDomainGenerator domGen;
 
 	// register tkd domain generator class
-	bridge::ExportedClass<domGen>& domgenC = reg->add_class_<domGen>(
-			"TKDDomainGenerator", grp, "Domain (grid) generator for tkds.");
+	reg.add_class_<domGen>("TKDDomainGenerator", grp, "Domain (grid) generator for tkds.")
+		.add_constructor<void (*)(Grid&, ISubsetHandler&)>(
+			"Grid to fill with TKD#SubsetHandler to use")
 
-	domgenC.add_constructor<void (*)(Grid&, ISubsetHandler&)>(
-			"Grid to fill with TKD#SubsetHandler to use");
-
-	domgenC.add_constructor<void (*)(Grid&, ISubsetHandler&, bool, bool)>(
+		.add_constructor<void (*)(Grid&, ISubsetHandler&, bool, bool)>(
 			"Grid to fill with TKD#SubsetHandler to use;"
-			"third parameter indicates whether a SC domain should be created");
+			"third parameter indicates whether a SC domain should be created")
 
-	domgenC.add_method("setIsSCDomain", &domGen::setSCDomain,
+		.add_method("setIsSCDomain", &domGen::setSCDomain,
 			"switch whether a stratum corneum domain or a simple tkd domain"
-			"will be created on createDomain()");
+			"will be created on createDomain()")
 
-	domgenC.add_method("setGridObject", &domGen::setGridObject,
-			"sets Grid and SubsetHandler to use.");
+		.add_method("setGridObject", &domGen::setGridObject,
+			"sets Grid and SubsetHandler to use.")
 
 	// register createSCDomain
-	domgenC.add_method(
+		.add_method(
 			"createSCDomain", &domGen::createSCDomain,
 			"fills your given grid and SubsetHandler with a stratum corneum domain "
 			" with given parameters a, h, w and d_lipid",
-			"a#w#h#d_lipid#rows#cols#layers");
+			"a#w#h#d_lipid#rows#cols#layers")
 	// register createSimpleTKDDomain
-	domgenC.add_method(
+		.add_method(
 			"createSimpleTKDDomain", &domGen::createSimpleTKDDomain,
 			"fills your given grid and SubsetHandler with tkds "
 			"with given parameters a, h, w and d_lipid",
-			"a#w#h#d_lipid#rows#cols#layers");
+			"a#w#h#d_lipid#rows#cols#layers")
 
 	// register setSubsetHandlerInfo()
-	domgenC.add_method("setSubsetInfo",
-			&domGen::setSubsetHandlerInfo,
+		.add_method("setSubsetInfo", &domGen::setSubsetHandlerInfo,
 			"Set SubsetHandler informations (name, color) for corneocytes and lipid matrix",
-			"corneocyte_name#lipid_name#corneocyte_color#lipid_color");
-
-	// register helper functions for subset indices as global functions
-	reg->add_function("CorneocyteIndex",
-			(int (*)(void)) (&domGen::getCorneocyteIndex),
-				"Get subset index for corneocytes");
-
-	reg->add_function("LipidIndex",
-			(int (*)(void)) &domGen::getLipidIndex,
-				"Get subset index for lipid matrix");
+			"corneocyte_name#lipid_name#corneocyte_color#lipid_color")
 
 	// register TKDGeometryGenerator& getGeometryGenerator() const
-	domgenC.add_method("getGeometryGenerator",
-			&domGen::getGeometryGenerator,
+		.add_method("getGeometryGenerator",	&domGen::getGeometryGenerator,
 			"Geometry generator used to build coordinate informations. \
 			 Also calculates volume and surface of single tkd");
 
-	// tkd geometry generator class
-	bridge::ExportedClass<geomGen>& geomGenC = reg->add_class_<geomGen>(
-			"TKDGeometryGenerator", grp, "Geometry generator class for tkds");
+	///////////////////////////////////////////////////////////////////////////
+	// TKD geometry generator class
+	reg.add_class_<geomGen>("TKDGeometryGenerator", grp,
+			"Geometry generator class for tkds")
 	// ctor
-	geomGenC.add_constructor<void (*)(number, number, number, number)>(
-			"height#baseEdgeLength#diameter#d_lipid");
+		.add_constructor<void (*)(number, number, number, number)>(
+			"height#baseEdgeLength#diameter#d_lipid")
 	// register createGeometry()
-	geomGenC.add_method("createGeometry", &geomGen::createGeometry);
+		.add_method("createGeometry", &geomGen::createGeometry)
 
 	// add volume methods
-	geomGenC.add_method("GetVolume",
+		.add_method("GetVolume",
 			(number (geomGen::*)(int) const) (&geomGen::getVolume),
-			"Volume of given subset (1 element)");
+			"Volume of given subset (1 element)")
+
+	// register surface methods
+		.add_method("GetSurface",
+			(number (geomGen::*)(int) const) (&geomGen::getSurface),
+			"Surface of given subset (1 element)");
+
+	///////////////////////////////////////////////////////////////////////////
 	// add static member function of TKDGeometryGenerator as global function
-	reg->add_function("GetVolume",
+	reg.add_function("GetVolume",
 			(number (*)(number, number, number)) (&geomGen::getVolume),
 			"calculate Volume for given geometrical parameters");
 
-	// register surface methods
-	geomGenC.add_method("GetSurface",
-			(number (geomGen::*)(int) const) (&geomGen::getSurface),
-			"Surface of given subset (1 element)");
+
 	// add static member function of TKDGeometryGenerator as global function
-	reg->add_function("GetSurface",
+	reg.add_function("GetSurface",
 			(number (*)(number, number, number)) (&geomGen::getSurface),
 			"calculate Surface for given geometrical parameters");
+
+	// register helper functions for subset indices as global functions
+	reg.add_function("CorneocyteIndex",
+			(int (*)(void)) (&domGen::getCorneocyteIndex),
+				"Get subset index for corneocytes");
+
+	reg.add_function("LipidIndex",
+			(int (*)(void)) &domGen::getLipidIndex,
+				"Get subset index for lipid matrix");
 }
+
+}; // end Functionality
+} // end namespace tkd
+
+// register tkd generator functions for usage in ug_script
+extern "C" void
+InitUGPlugin_TKDGenerator(Registry* reg, string grp) {
+	grp.append("/tkd");
+	typedef tkd::Functionality Functionality;
+	typedef boost::mpl::list<Domain3d> compile3d;
+
+#ifdef UG_DIM_3
+	try {
+		RegisterDomainDependent<Functionality, compile3d>(*reg, grp);
+	} UG_REGISTRY_CATCH_THROW(grp);
+#else
+	UG_WARNING("UG has to be compiled with 3d support "
+			"for usage of plugin TKDGenerator.");
+#endif
+}
+
+} // end namespace ug
